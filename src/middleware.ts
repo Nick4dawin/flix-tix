1e11ebac17e015607e81d8d398d955e904d47f9e
Sagnik <sagnik.bhattacharjee099@gmail.com>
2024-04-17 16:22:23 +0530

    Init, Added DB, Clerk Auth

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import { authMiddleware } from "@clerk/nextjs";
+export default authMiddleware({
+    publicRoutes: [
+        '/', "/(api|trpc)(.*)" , '/favicon.ico'
+    ],
+    ignoredRoutes:[],
+});
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/","/(api|trpc)(.*)"],
+};
